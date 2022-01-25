b9c4f8c92d2ef9ef01b884036037203fdc1ff4f3
Lucy Lee <leelucy412@gmail.com>
2022-01-25 01:59:28 -0500

    Create entry point in react app

---
+++ meopets/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import App from './components/App';
+import './index.css';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById('root')
+);

8c1ddd577a22f75451cc78af72bd59352a520cb5
rivka99 <rivkafl99@gmail.com>
2022-01-25 14:58:05 -0700

    added more functionality to UserProfile, issue #3

--- meopets/src/components/UserProfile.js
+++ meopets/src/components/UserProfile.js
@@ -1,9 +1,17 @@
 import { useState } from "react"
+import { NavLink } from "react-router-dom"
+import Pets from "./Pets"
 export default function UserProfile(props){
     const [currency, setCurrency] = useState(300)
-    const  [greeting, setGreeting] = useState("Welcome Bobble!")
+    const  [greeting, setGreeting] = useState("Welcome Back, Bobble!")
   return (
       <div>
+           {/* Header to be replaced w/ <NavLink className="nav" /> */}
+          <header className="nav">
+            <p className="nav-txt">meopets</p>
+          </header>
+
+          <div className="user-disp">
           <div className="currency">
               <img src="https://cdn-icons-png.flaticon.com/512/550/550638.png" alt="coin stack" className="coins"/> 
               <p>{currency}</p>
@@ -11,6 +19,8 @@ export default function UserProfile(props){
           <div className="prof-msg">
               <h4 className="prof-msg-txt">{greeting}</h4>
           </div>
+          </div>
+          <Pets/>
       </div>
    
   )

4f249cf6b79f281b7a295daaf9063da22abcfb6f
Lucy Lee <leelucy412@gmail.com>
2022-01-25 23:17:19 -0500

    Navigate to user profile on form submit

--- meopets/src/components/Login.js
+++ meopets/src/components/Login.js
@@ -1,13 +1,27 @@
-export default function Login(){
-    //usenavigate => Userprofile
+import React from "react";
+import {useNavigate} from "react-router-dom";
+
+export default function Login() {
+    let navigate = useNavigate()
+
+    // const [user, setUser] = React.useState({
+    //     username: "",
+    //     password: ""
+    // });
+
     return(
-        <div>
-             <label htmlFor="userName">User Name</label>
-             <input type="text" id="user" name="userName"  />
-             <label htmlFor="password">User Name</label>
-             <input type="text" id="user" name="password"  />
-             {/* go to user profile */}
-             <button ></button>
-        </div>
+        <form onSubmit={() => {
+            navigate("/userprofile")
+        }}>
+             <label>Username:<br/>
+                 <input type="text" name="username" />
+             </label><br/>
+
+             <label>Password:<br/>
+             <input type="text" name="password"  />
+             </label><br/>
+
+            <input type="submit" value="Submit"/>
+        </form>
     )
 }

857f3aa998b3dae02ad58dfb53015ae8d2f320aa
Lucy Lee <leelucy412@gmail.com>
2022-01-26 01:38:12 -0500

    Comment with code explanation

--- meopets/src/components/App.js
+++ meopets/src/components/App.js
@@ -3,19 +3,26 @@ import '../App.css';
 import UserProfile from './UserProfile';
 
 function App() {
-
     return (
         <>
+            {/* Todo: Please move nav styling into App.css */}
             <nav style={{
                 backgroundColor: "pink",
                 paddingBottom: "15px",
                 paddingTop: "15px",
                 marginBottom: 30
-
             }}>
-                <h1>Meopets</h1>
+                <Link to={`/home`}>
+                    <h1>Meopets</h1> {/* Click on logo to redirect to Home */}
+                </Link>
             </nav>
 
+            {/* Outlet sets this component as the parent route:
+
+            App will be the main parent route for all pages on this site, path = "/"
+            The navigation bar will always render, with all its links
+            This makes it so that we don't have to copy and paste the navbar HTML
+            over and over again */}
             <Outlet />
         </>
     );

fd2bdf6b8b49980ac535084e0d6c5a1bd8e6ddcd
Lucy Lee <leelucy412@gmail.com>
2022-01-26 00:28:25 -0500

    Fix merge conflict on userProfile

--- meopets/src/components/UserProfile.js
+++ meopets/src/components/UserProfile.js
@@ -1,28 +1,26 @@
 import { useState } from "react"
-import { NavLink } from "react-router-dom"
 import Pets from "./Pets"
-export default function UserProfile(props){
-    const [currency, setCurrency] = useState(300)
-    const  [greeting, setGreeting] = useState("Welcome Back, Bobble!")
-  return (
-      <div>
-           {/* Header to be replaced w/ <NavLink className="nav" /> */}
-          <header className="nav">
-            <p className="nav-txt">meopets</p>
-          </header>
+import {Outlet} from "react-router-dom";
 
-          <div className="user-disp">
-          <div className="currency">
-              <img src="https://cdn-icons-png.flaticon.com/512/550/550638.png" alt="coin stack" className="coins"/> 
-              <p>{currency}</p>
-          </div>
-          <div className="prof-msg">
-              <h4 className="prof-msg-txt">{greeting}</h4>
-          </div>
-          </div>
-          <Pets/>
-      </div>
-   
-  )
-  
+export default function UserProfile(props) {
+    const [currency, setCurrency] = useState(300)
+    const [greeting, setGreeting] = useState("Welcome Back, Bobble!")
+
+    return (
+        <>
+            <div className="user-disp">
+                <div className="currency">
+                    <img src="https://cdn-icons-png.flaticon.com/512/550/550638.png" alt="coin stack"
+                         className="coins"/>
+                    <p>{currency}</p>
+                </div>
+                <div className="prof-msg">
+                    <h4 className="prof-msg-txt">{greeting}</h4>
+                </div>
+            </div>
+            <Pets/>
+
+            <Outlet/>
+        </>
+    );
 }

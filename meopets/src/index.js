3ab4402e2c6e3674ca551f0f2fa1b66125526c4e
Lucy Lee <leelucy412@gmail.com>
2022-01-26 21:41:57 -0500

    Add path for Home

--- meopets/src/index.js
+++ meopets/src/index.js
@@ -26,6 +26,7 @@ ReactDOM.render(
             <Routes>
                 <Route path="/" element={<App />} >
                     <Route index element={<Home />} />
+                    <Route path="home" element={<Home />} />
                     <Route path="login" element={<Login />} />
                     <Route path="signup" element={<SignUp />} />
                     <Route path="userprofile" element={<UserProfile />} />

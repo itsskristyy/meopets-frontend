1e30dec9f888e599e182aea7f9c8e4bdac536f5d
Lucy Lee <leelucy412@gmail.com>
2022-01-28 04:17:04 -0500

    Temporarily remove daily coins button

--- meopets/src/components/UserProfile.js
+++ meopets/src/components/UserProfile.js
@@ -15,41 +15,46 @@ export default function UserProfile(props) {
         }
     }, []);
 
-    async function getCoins(newCurrency) {
-        return updateUser(newCurrency);
-    }
+    // async function getCoins(newCurrency) {
+    //     return updateUser(newCurrency);
+    // }
 
     return (
         <>  
             {!user.isLoggedIn && navigate('/home')}
-            {user.isLoggedIn && 
-            <div className="user-disp">
-                <button type='button' 
-                    onClick={async () => {
-                        const today = new Date();
-                        today.setHours(0, 0, 0, 0);
-                        const lastUpdated = new Date(user.user.lastUpdated);
-                        if(today > lastUpdated) {
-                            await getCoins({currency: user.currency + 50})
-                        } else {
+            {user.isLoggedIn &&
+                <div>
+                    <div className="currency">
+                        <img src="https://cdn-icons-png.flaticon.com/512/550/550638.png" alt="coin stack"
+                             className="coins-img"/>
+                        <p className="coins">{user.currency + 50}</p>
+                    </div>
 
-                        }
-                    }}>Get Your Daily Coins!</button>
-                <div className="currency">
-                    <img src="https://cdn-icons-png.flaticon.com/512/550/550638.png" alt="coin stack"
-                         className="coins-img"/>
-                    <p className="coins">{user.currency}</p>
-                </div>
-                <div className="prof-msg">
+                    <div className="banner">
+
+                {/*<button type='button' */}
+                {/*    onClick={async () => {*/}
+                {/*        const today = new Date();*/}
+                {/*        today.setHours(0, 0, 0, 0);*/}
+                {/*        const lastUpdated = new Date(user.user.lastUpdated);*/}
+                {/*        if(today > lastUpdated) {*/}
+                {/*            await getCoins({currency: user.currency + 50})*/}
+                {/*        } else {*/}
+
+                {/*        }*/}
+                {/*    }}>Get Your Daily Coins!</button>*/}
+
+                <div>
                     {user.user.created !== user.user.lastUpdated &&
-                    <h4 className="prof-msg-txt">Welcome back, {user.user.username}</h4>}
+                    <h3 className="prof-msg-txt">Welcome back, {user.user.username}!</h3>}
                     {user.user.created === user.user.lastUpdated &&
                     <h4 className="prof-msg-txt">Welcome, {user.user.username}!</h4>}
                 </div>
+
+            </div>
             </div>}
 
-
-            <Pets/> {/* Render grid cells/flexboxes of all pets the user owns */}
+            <Pets/>
 
             {/* Outlet sets this component as the parent route:
             
